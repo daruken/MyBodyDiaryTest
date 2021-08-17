ae4f7328f7266d4b302ffd4957d411b312749f30
N22103011-JHR <n22103011-jhr@N22103011-JHR.local>
2021-08-17 17:32:05 +0900

    Add show user list.

--- mybody_react/src/pages/User.js
+++ mybody_react/src/pages/User.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
-async function getAllUsers() {
-  const response = await axios.get('/users');
+class User extends Component {
+  constructor(props) {
+    super(props);
+  };
 
-  return response.data;
-}
+  state = {
+    user : []
+  };
 
-const User = () => {
-  return (
-    <div>
-      Hi Users!
+  componentDidMount() {
+    console.log('in componentDidMount');
+    this.getUsers();
+    console.log('after user : ' + JSON.stringify(this.state.user));
+  }
 
+  getUsers = async() => {
+    const response = await axios.get('/users');
+    const user = response.data;
+    console.log('in user : ' + JSON.stringify(user));
+
+    this.setState({user});
+  };
+
+  render() {
+    return <div>
+      <h2>User Page</h2>
+      
+      <p>User list</p>
+      {
+      this.state.user.map((user) => {
+        return [
+          <p>ID : { user.id }</p>,
+          <p>Email : { user.email }</p>
+        ];
+      })
+      }
     </div>
-  )
+  }
 }
 
 export default User;
